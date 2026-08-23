50119c4fe0049547495caef61d8e5a40ed23fb7e
Athul Sathyan <athulsathyan136@gmail.com>
2026-08-23 20:54:20 +0530

    🚀 Day 1: Hello World & Variables

---
+++ day_01_hello_world.py
@@ -0,0 +1,13 @@
+print("Hello, World!")
+name='Athul Sathyan'
+age=23
+cgpa=8.0
+print(f"My name is {name}")
+print(f'I am {age} years old')
+print(f'My CGPA is {cgpa}')
+
+print("What is your name ?",name)
+print("How old are you ?",age)
+
+user_name=input("what is your name ? ")
+user_name=input("How old are you")
