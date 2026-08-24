5f79297de9a8ef4c724904681c91b16b11034360
Athul Sathyan <athulsathyan136@gmail.com>
2026-08-24 20:40:15 +0530

    ✅ Day 2: Data Types

---
+++ practice/day_02_datatypes.py
@@ -0,0 +1,15 @@
+# Day 2: Simple Data Types
+age = 22
+price = 99.99
+name = "Athul"
+is_learning = True
+
+print(age, type(age))
+print(price, type(price))
+print(name, type(name))
+print(is_learning, type(is_learning))
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+total = int(num1) + int(num2)
+print("Total is:", total)
