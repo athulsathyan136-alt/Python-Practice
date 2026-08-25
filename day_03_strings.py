7cefd77806d2c39e445bad0089be86fd937914af
Athul Sathyan <athulsathyan136@gmail.com>
2026-08-25 22:50:12 +0530

    ✅ Day 3: String Manipulation

---
+++ day_03_strings.py
@@ -0,0 +1,12 @@
+text = "python is awesome"
+
+print("Original: ",text)
+print("Uppercase: ",text.upper())
+print("Lowercase: ",text.lower())
+print("Capitalize: ",text.capitalize())
+print("Title case: ",text.title())
+
+sentence="I Love Building AI Systems"
+words = sentence.split()
+print("\nWords in sentence:", words)
+print("Total words:", len(words))
